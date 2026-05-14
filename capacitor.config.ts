379611172a78f148e8cfd3cf76156ee7262d3c98
ConyFreya180993 <conyfreyatecnico@gmail.com>
2026-05-13 21:50:52 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TablonAvisos',
+  webDir: 'www'
+};
+
+export default config;
